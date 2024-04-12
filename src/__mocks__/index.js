cfce73c5165652de27e7b402f7f0bb07baeda1e7
lodev09 <lodev09@gmail.com>
2024-04-12 15:03:45 +0800

    test: fix mock

--- src/__mocks__/index.js
+++ src/__mocks__/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { View } from 'react-native'
 
+export * from '../TrueSheetGrabber'
+
 export class TrueSheet extends React.Component {
   dismiss = jest.fn()
   present = jest.fn()
+  resize = jest.fn()
 
   render() {
     return <View {...this.props} />
