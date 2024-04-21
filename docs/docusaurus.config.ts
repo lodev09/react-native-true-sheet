fdddf90f0d7437408c5c737ddc44b14f692d9093
lodev09 <lodev09@gmail.com>
2024-04-22 01:21:59 +0800

    docs: baseUrl

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   tagline: 'The true native bottom sheet 💩',
   favicon: 'img/favicon.png',
   url: 'https://sheet.lodev09.com',
-  baseUrl: '/react-native-true-sheet/',
+  baseUrl: '/',
   organizationName: 'lodev09',
   projectName: 'react-native-true-sheet',
   onBrokenLinks: 'throw',
