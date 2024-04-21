8973b3452f909fa45834a71816d0f624be5606e6
lodev09 <lodev09@gmail.com>
2024-04-21 18:02:24 +0800

    docs: add docs

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,84 @@
+import { themes as prismThemes } from 'prism-react-renderer'
+import type { Config } from '@docusaurus/types'
+import type * as Preset from '@docusaurus/preset-classic'
+
+const config: Config = {
+  title: 'React Native True Sheet',
+  tagline: 'The true native bottom sheet 💩',
+  favicon: 'img/favicon.png',
+  url: 'https://truesheet.lodev09.com',
+  baseUrl: '/',
+  organizationName: 'lodev09',
+  projectName: 'react-native-true-sheet',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          editUrl: 'https://github.com/lodev09/react-native-true-sheet/main/docs/',
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'True Sheet',
+      logo: {
+        alt: 'TrueSheet Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'trueSheetSidebar',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          to: '/blog',
+          label: 'Blog',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/lodev09/react-native-true-sheet/tree/main/example',
+          label: 'Example',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/lodev09/react-native-true-sheet',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      copyright: `Made with ❤️ by <a href="https://github.com/lodev09">Jovanni Lo</a>`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.palenight,
+      additionalLanguages: ['swift', 'java', 'kotlin'],
+    },
+  } satisfies Preset.ThemeConfig,
+}
+
+export default config
