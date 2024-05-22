b56c09f7548b5bdff369ec296601582da0d78ea5
lodev09 <lodev09@gmail.com>
2024-05-23 07:11:12 +0800

    fix: fix android footer position bug

--- android/src/main/java/com/lodev09/truesheet/core/Utils.kt
+++ android/src/main/java/com/lodev09/truesheet/core/Utils.kt
@@ -1,32 +1,61 @@
 package com.lodev09.truesheet.core
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
 import android.util.DisplayMetrics
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.uimanager.PixelUtil
 
 object Utils {
   @SuppressLint("DiscouragedApi", "InternalInsetResource")
-  fun screenHeight(reactContext: ReactContext): Int {
-    val activity = reactContext.currentActivity ?: return 0
-
-    // Get the screen metrics
+  fun screenHeight(context: ReactContext): Int {
+    val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
     val displayMetrics = DisplayMetrics()
-    activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
-    val screenHeight = displayMetrics.heightPixels
 
-    val resources = activity.resources
-
-    // Calculate status bar height
-    var statusBarHeight = 0
-    val resourceId: Int = resources.getIdentifier("status_bar_height", "dimen", "android")
-    if (resourceId > 0) {
-      statusBarHeight = resources.getDimensionPixelSize(resourceId)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      context.display?.getRealMetrics(displayMetrics)
+    } else {
+      windowManager.defaultDisplay.getMetrics(displayMetrics)
     }
 
-    // Calculate max usable height
-    return screenHeight - statusBarHeight
+    val screenHeight = displayMetrics.heightPixels
+
+    // Subtract status bar height
+    val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
+    val statusBarHeight = if (resourceId > 0) {
+      context.resources.getDimensionPixelSize(resourceId)
+    } else {
+      0
+    }
+
+    // Subtract navigation bar height (if present)
+    val navigationBarHeight: Int
+    val hasNavigationBar = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      val windowInsets = context.getSystemService(WindowManager::class.java)
+        ?.currentWindowMetrics
+        ?.windowInsets
+      windowInsets?.isVisible(WindowInsets.Type.navigationBars()) ?: false
+    } else {
+      val resourceIdNav = context.resources.getIdentifier("navigation_bar_height", "dimen", "android")
+      resourceIdNav > 0
+    }
+
+    navigationBarHeight = if (hasNavigationBar) {
+      val resourceIdNav = context.resources.getIdentifier("navigation_bar_height", "dimen", "android")
+      if (resourceIdNav > 0) {
+        context.resources.getDimensionPixelSize(resourceIdNav)
+      } else {
+        0
+      }
+    } else {
+      0
+    }
+
+    return screenHeight - statusBarHeight - navigationBarHeight
   }
 
   fun toDIP(value: Int): Float = PixelUtil.toDIPFromPixel(value.toFloat())
