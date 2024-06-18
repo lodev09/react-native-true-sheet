a3285d0b00783c4ed9c2be12ab5ed5d167f178c1
Jovanni Lo <lodev09@gmail.com>
2024-06-18 12:25:26 +0800

    fix(android): `FlatList` with `TextInput` resize bug (#42)
    * chore: update example
    
    * feat(android): implement `keyboardMode` prop
    
    * docs: add `keyboardMode` prop reference
    
    * chore: example
    
    * chore: bump deps

---
+++ example/src/components/Input.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { TextInput, View, type TextStyle, type ViewStyle } from 'react-native'
+
+import { BORDER_RADIUS, GRAY, INPUT_HEIGHT, SPACING } from '../utils'
+
+export const Input = () => {
+  return (
+    <View style={$inputContainer}>
+      <TextInput style={$input} placeholder="Enter some text..." placeholderTextColor={GRAY} />
+    </View>
+  )
+}
+
+const $inputContainer: ViewStyle = {
+  backgroundColor: 'white',
+  paddingHorizontal: SPACING,
+  height: INPUT_HEIGHT,
+  borderRadius: BORDER_RADIUS,
+  justifyContent: 'center',
+  marginBottom: SPACING,
+}
+
+const $input: TextStyle = {
+  fontSize: 16,
+  height: INPUT_HEIGHT,
+}
--- example/src/sheets/FlatListSheet.tsx
+++ example/src/sheets/FlatListSheet.tsx
@@ -1,9 +1,9 @@
 import React, { forwardRef, useRef, type Ref } from 'react'
-import { FlatList, type ViewStyle } from 'react-native'
+import { FlatList, View, type ViewStyle } from 'react-native'
 import { TrueSheet, type TrueSheetProps } from '@lodev09/react-native-true-sheet'
 
-import { SPACING, times } from '../utils'
-import { DemoContent } from '../components'
+import { DARK, DARK_GRAY, INPUT_HEIGHT, SPACING, times } from '../utils'
+import { DemoContent, Input } from '../components'
 
 interface FlatListSheetProps extends TrueSheetProps {}
 
@@ -14,21 +14,24 @@ export const FlatListSheet = forwardRef((props: FlatListSheetProps, ref: Ref<Tru
     <TrueSheet
       ref={ref}
       scrollRef={flatListRef}
-      sizes={['large']}
-      cornerRadius={24}
-      grabber={false}
-      maxHeight={600}
+      sizes={['medium', 'large']}
+      blurTint="dark"
+      backgroundColor={DARK}
+      keyboardMode="pan"
       onDismiss={() => console.log('Sheet FlatList dismissed!')}
       onPresent={() => console.log(`Sheet FlatList presented!`)}
       {...props}
     >
+      <View style={$header}>
+        <Input />
+      </View>
       <FlatList<number>
         ref={flatListRef}
         nestedScrollEnabled
         data={times(50, (i) => i)}
         contentContainerStyle={$content}
         indicatorStyle="black"
-        renderItem={() => <DemoContent radius={24} />}
+        renderItem={() => <DemoContent color={DARK_GRAY} />}
       />
     </TrueSheet>
   )
@@ -38,4 +41,16 @@ FlatListSheet.displayName = 'FlatListSheet'
 
 const $content: ViewStyle = {
   padding: SPACING,
+  paddingTop: INPUT_HEIGHT + SPACING * 4,
+}
+
+const $header: ViewStyle = {
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  backgroundColor: DARK,
+  paddingTop: SPACING * 2,
+  paddingHorizontal: SPACING,
+  zIndex: 1,
 }
