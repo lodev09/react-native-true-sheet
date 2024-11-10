9e4988d18933f368b55faffd7a5886d95ec1659d
Mathieu Acthernoene <zoontek@gmail.com>
2024-11-10 22:17:00 +0100

    fix: add onStart

--- android/src/main/java/com/lodev09/truesheet/TrueSheetDialog.kt
+++ android/src/main/java/com/lodev09/truesheet/TrueSheetDialog.kt
@@ -22,10 +22,6 @@ class TrueSheetDialog(private val reactContext: ThemedReactContext, private val 
   private var sheetView: ViewGroup
   private var windowAnimation: Int = 0
 
-  override fun getEdgeToEdgeEnabled(): Boolean {
-    return if (Utils.EDGE_TO_EDGE) true else super.getEdgeToEdgeEnabled()
-  }
-
   /**
    * Specify whether the sheet background is dimmed.
    * Set to `false` to allow interaction with the background components.
@@ -72,6 +68,25 @@ class TrueSheetDialog(private val reactContext: ThemedReactContext, private val 
 
     // Update the usable sheet height
     maxScreenHeight = Utils.screenHeight(reactContext)
+  }
+
+  override fun getEdgeToEdgeEnabled(): Boolean {
+    return if (Utils.EDGE_TO_EDGE) true else super.getEdgeToEdgeEnabled()
+  }
+
+  override fun onStart() {
+    super.onStart()
+
+    if (Utils.EDGE_TO_EDGE) {
+      window?.apply {
+        setFlags(
+          WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+          WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      }
+    }
   }
 
   /**
