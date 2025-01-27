c990fd668e844667b741915bd380a9734d2bbbc3
Rickard Natt och Dag <rickard.nattochdag@gmail.com>
2025-01-27 09:17:49 +0100

    chore: add suppression comment for react native < 0.77

--- android/src/main/java/com/lodev09/truesheet/TrueSheetViewManager.kt
+++ android/src/main/java/com/lodev09/truesheet/TrueSheetViewManager.kt
@@ -107,6 +107,9 @@ class TrueSheetViewManager : ViewGroupManager<TrueSheetView>() {
     for (i in 0 until minOf(sizes.size(), 3)) {
       when (sizes.getType(i)) {
         ReadableType.Number -> result.add(sizes.getDouble(i))
+        // React Native < 0.77 used String for getString, but 0.77
+        // changed it to String?. Suppress the error for older APIs.
+        @Suppress("UNNECESSARY_SAFE_CALL")
         ReadableType.String -> sizes.getString(i)?.let { result.add(it) }
         else -> Log.d(TAG, "Invalid type")
       }
