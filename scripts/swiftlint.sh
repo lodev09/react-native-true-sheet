0963dac82aab20dd40a5442b68332f82a844be05
lodev09 <lodev09@gmail.com>
2025-11-16 15:56:56 +0800

    refactor: replace swiftlint with clang-format for Objective-C

--- scripts/swiftlint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if which swiftlint >/dev/null; then
-  cd ios && swiftlint --quiet --fix && swiftlint --quiet && swiftformat --quiet .
-else
-  echo "error: SwiftLint or SwiftFormat not installed, install with 'brew install swiftlint' (or manually from https://github.com/realm/SwiftLint)"
-  exit 1
-fi
