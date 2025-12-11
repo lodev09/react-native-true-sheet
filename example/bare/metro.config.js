c865e4db5f0c48e8f220d835e559fd2348b082f8
lodev09 <lodev09@gmail.com>
2025-12-12 02:01:44 +0800

    fix: update config paths for new example folder structure

--- example/bare/metro.config.js
+++ example/bare/metro.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 const { getDefaultConfig } = require('@react-native/metro-config')
 const { withMetroConfig } = require('react-native-monorepo-config')
 
-const root = path.resolve(__dirname, '..')
-const pkg = require('../package.json')
+const root = path.resolve(__dirname, '../..')
+const pkg = require('../../package.json')
 
 /**
  * Metro configuration
