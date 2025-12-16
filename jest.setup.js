aa0a0cf5029b99535f2bce625ea73eb34a41d6b7
lodev09 <lodev09@gmail.com>
2025-12-16 15:17:43 +0800

    chore: simplify jest.setup.js mocks

--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,11 @@
 /* eslint-env jest */
+
 // Mock the native module
 jest.mock('./src/specs/NativeTrueSheetModule', () => ({
   __esModule: true,
   default: {
-    present: jest.fn(),
-    dismiss: jest.fn(),
-    dismissAll: jest.fn(),
+    presentByRef: jest.fn(),
+    dismissByRef: jest.fn(),
   },
 }));
 
@@ -13,7 +13,7 @@ jest.mock('./src/specs/NativeTrueSheetModule', () => ({
 jest.mock('./src/fabric/TrueSheetViewNativeComponent', () => {
   const React = require('react');
   const { View } = require('react-native');
-  
+
   return {
     __esModule: true,
     default: React.forwardRef((props, ref) => {
@@ -25,7 +25,7 @@ jest.mock('./src/fabric/TrueSheetViewNativeComponent', () => {
 jest.mock('./src/fabric/TrueSheetContainerViewNativeComponent', () => {
   const React = require('react');
   const { View } = require('react-native');
-  
+
   return {
     __esModule: true,
     default: React.forwardRef((props, ref) => {
@@ -37,7 +37,7 @@ jest.mock('./src/fabric/TrueSheetContainerViewNativeComponent', () => {
 jest.mock('./src/fabric/TrueSheetContentViewNativeComponent', () => {
   const React = require('react');
   const { ScrollView } = require('react-native');
-  
+
   return {
     __esModule: true,
     default: React.forwardRef((props, ref) => {
@@ -46,10 +46,10 @@ jest.mock('./src/fabric/TrueSheetContentViewNativeComponent', () => {
   };
 });
 
-jest.mock('./src/fabric/TrueSheetFooterViewNativeComponent', () => {
+jest.mock('./src/fabric/TrueSheetHeaderViewNativeComponent', () => {
   const React = require('react');
   const { View } = require('react-native');
-  
+
   return {
     __esModule: true,
     default: React.forwardRef((props, ref) => {
@@ -58,47 +58,45 @@ jest.mock('./src/fabric/TrueSheetFooterViewNativeComponent', () => {
   };
 });
 
-// Mock reanimated if it's being used
-jest.mock('react-native-reanimated', () => {
+jest.mock('./src/fabric/TrueSheetFooterViewNativeComponent', () => {
+  const React = require('react');
+  const { View } = require('react-native');
+
   return {
-    default: {
-      call: () => {},
-      createAnimatedComponent: (component) => component,
-    },
-    useSharedValue: jest.fn(() => ({ value: 0 })),
-    useAnimatedStyle: jest.fn((callback) => callback()),
-    withTiming: jest.fn((value) => value),
-    withSpring: jest.fn((value) => value),
-    runOnJS: jest.fn((fn) => fn),
-    createAnimatedComponent: (component) => component,
-    Easing: {
-      bezier: jest.fn(() => jest.fn()),
-      linear: jest.fn(),
-      ease: jest.fn(),
-      quad: jest.fn(),
-      cubic: jest.fn(),
-      sin: jest.fn(),
-      circle: jest.fn(),
-      exp: jest.fn(),
-      elastic: jest.fn(),
-      back: jest.fn(),
-      bounce: jest.fn(),
-      in: jest.fn(),
-      out: jest.fn(),
-      inOut: jest.fn(),
-    },
+    __esModule: true,
+    default: React.forwardRef((props, ref) => {
+      return React.createElement(View, { ...props, ref });
+    }),
   };
 });
 
-// Mock worklets if it's being used
-jest.mock('react-native-worklets-core', () => ({}), { virtual: true });
+// Mock react-native-reanimated
+jest.mock('react-native-reanimated', () => ({
+  default: {
+    call: () => {},
+    createAnimatedComponent: (component) => component,
+  },
+  useSharedValue: jest.fn((initial) => ({ value: initial })),
+  useAnimatedStyle: jest.fn((callback) => callback()),
+  withTiming: jest.fn((value) => value),
+  withSpring: jest.fn((value) => value),
+  runOnJS: jest.fn((fn) => fn),
+  createAnimatedComponent: (component) => component,
+  useEvent: jest.fn(() => jest.fn()),
+  useHandler: jest.fn(() => ({ context: {}, doDependenciesDiffer: false })),
+  Easing: {
+    bezier: jest.fn(() => jest.fn()),
+  },
+}));
 
+// Mock react-native-worklets
+jest.mock('react-native-worklets-core', () => ({}), { virtual: true });
 jest.mock(
   'react-native-worklets',
   () => ({
     scheduleOnRN: jest.fn((fn) => fn),
     scheduleOnJS: jest.fn((fn) => fn),
-    useSharedValue: jest.fn(() => ({ value: 0 })),
+    useSharedValue: jest.fn((initial) => ({ value: initial })),
     useWorklet: jest.fn((fn) => fn),
     runOnJS: jest.fn((fn) => fn),
     runOnUI: jest.fn((fn) => fn),
