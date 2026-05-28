27568d08ad97d9c8aa54f44c48f627d439c3c6fe
Pekaz <afakafak@gmail.com>
2026-05-28 12:20:41 +0800

    fix(ios): smooth sheet position jump when keyboard dismisses with focus outside sheet

--- ios/core/TrueSheetKeyboardObserver.mm
+++ ios/core/TrueSheetKeyboardObserver.mm
@@ -12,9 +12,18 @@
 #import "../TrueSheetViewController.h"
 #import "../utils/UIView+FirstResponder.h"
 
+static const CGFloat kMinSmoothingOffset = 1.0;
+static const NSTimeInterval kFallbackAnimationDuration = 0.25;
+
 @implementation TrueSheetKeyboardObserver {
   NSHashTable<id<TrueSheetKeyboardObserverDelegate>> *_delegates;
   CGFloat _currentHeight;
+  BOOL _isKeyboardVisibleForSheet;
+  BOOL _isSmoothingKeyboardHide;
+  BOOL _isHandlingHide;
+  __weak UIView *_smoothingPresentedView;
+  CGAffineTransform _smoothingOriginalTransform;
+  NSUInteger _smoothingToken;
 }
 
 - (CGFloat)currentHeight {
@@ -24,6 +33,7 @@
 - (instancetype)init {
   if (self = [super init]) {
     _delegates = [NSHashTable weakObjectsHashTable];
+    _smoothingOriginalTransform = CGAffineTransformIdentity;
   }
   return self;
 }
@@ -38,13 +48,32 @@
 
 - (void)start {
   [[NSNotificationCenter defaultCenter] addObserver:self
-                                           selector:@selector(keyboardWillChangeFrame:)
+                                           selector:@selector(handleKeyboardNotification:)
                                                name:UIKeyboardWillChangeFrameNotification
+                                             object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(handleKeyboardNotification:)
+                                               name:UIKeyboardWillHideNotification
                                              object:nil];
 }
 
 - (void)stop {
   [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillChangeFrameNotification object:nil];
+  [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+
+  ++_smoothingToken;
+  UIView *presented = _smoothingPresentedView;
+  if (presented && _isSmoothingKeyboardHide) {
+    [presented.layer removeAllAnimations];
+    presented.transform = _smoothingOriginalTransform;
+  }
+
+  _currentHeight = 0;
+  _isKeyboardVisibleForSheet = NO;
+  _isSmoothingKeyboardHide = NO;
+  _isHandlingHide = NO;
+  _smoothingPresentedView = nil;
+  _smoothingOriginalTransform = CGAffineTransformIdentity;
 }
 
 - (void)dealloc {
@@ -59,12 +88,82 @@
   return firstResponder != nil;
 }
 
-- (void)keyboardWillChangeFrame:(NSNotification *)notification {
-  if (_viewController && !_viewController.isTopmostPresentedController) {
+- (void)smoothKeyboardHideWithDuration:(NSTimeInterval)duration curve:(UIViewAnimationOptions)curve {
+  if (_isSmoothingKeyboardHide || !_viewController) {
     return;
   }
 
-  if (![self isFirstResponderWithinSheet]) {
+  UIView *presented = _viewController.presentationController.presentedView;
+  if (!presented) {
+    return;
+  }
+
+  // presentationLayer is nil when no animation is in flight; in that case the
+  // model frame already matches what's on screen, so the offset below resolves to 0.
+  CALayer *presentationLayer = presented.layer.presentationLayer;
+  CGFloat visualY = presentationLayer ? presentationLayer.frame.origin.y : presented.frame.origin.y;
+
+  _isSmoothingKeyboardHide = YES;
+  _smoothingPresentedView = presented;
+  _smoothingOriginalTransform = presented.transform;
+  NSUInteger token = ++_smoothingToken;
+
+  __weak __typeof(self) weakSelf = self;
+  __weak UIView *weakPresented = presented;
+
+  dispatch_async(dispatch_get_main_queue(), ^{
+    __strong __typeof(weakSelf) strongSelf = weakSelf;
+    if (!strongSelf || strongSelf->_smoothingToken != token) {
+      return;
+    }
+
+    UIView *strongPresented = weakPresented;
+    if (!strongPresented) {
+      strongSelf->_isSmoothingKeyboardHide = NO;
+      strongSelf->_smoothingPresentedView = nil;
+      return;
+    }
+
+    [strongPresented layoutIfNeeded];
+    CGFloat snappedY = strongPresented.frame.origin.y;
+    CGFloat offset = visualY - snappedY;
+    if (fabs(offset) < kMinSmoothingOffset) {
+      strongSelf->_isSmoothingKeyboardHide = NO;
+      strongSelf->_smoothingPresentedView = nil;
+      return;
+    }
+
+    CGAffineTransform original = strongSelf->_smoothingOriginalTransform;
+    CGAffineTransform translated =
+      CGAffineTransformConcat(original, CGAffineTransformMakeTranslation(0, offset));
+    strongPresented.transform = translated;
+
+    UIViewAnimationOptions opts =
+      curve | UIViewAnimationOptionBeginFromCurrentState
+      | UIViewAnimationOptionOverrideInheritedDuration
+      | UIViewAnimationOptionOverrideInheritedCurve
+      | UIViewAnimationOptionAllowUserInteraction;
+
+    NSTimeInterval animationDuration = duration > 0 ? duration : kFallbackAnimationDuration;
+    [UIView animateWithDuration:animationDuration
+                          delay:0
+                        options:opts
+                     animations:^{
+                       strongPresented.transform = original;
+                     }
+                     completion:^(__unused BOOL finished) {
+                       __strong __typeof(weakSelf) innerSelf = weakSelf;
+                       if (!innerSelf || innerSelf->_smoothingToken != token) {
+                         return;
+                       }
+                       innerSelf->_isSmoothingKeyboardHide = NO;
+                       innerSelf->_smoothingPresentedView = nil;
+                     }];
+  });
+}
+
+- (void)handleKeyboardNotification:(NSNotification *)notification {
+  if (_viewController && !_viewController.isTopmostPresentedController) {
     return;
   }
 
@@ -80,13 +179,45 @@
 
   CGRect keyboardFrameInWindow = [window convertRect:keyboardFrame fromWindow:nil];
   CGFloat keyboardHeight = MAX(0, window.bounds.size.height - keyboardFrameInWindow.origin.y);
+  BOOL isHideNotification = [notification.name isEqualToString:UIKeyboardWillHideNotification];
+  BOOL isKeyboardHiding = isHideNotification || keyboardHeight <= 0;
+  BOOL firstResponderWithinSheet = [self isFirstResponderWithinSheet];
+  BOOL wasKeyboardVisibleForSheet = _isKeyboardVisibleForSheet || _currentHeight > 0;
+  BOOL shouldEmitShow = !isKeyboardHiding
+    && keyboardHeight > 0
+    && (firstResponderWithinSheet || wasKeyboardVisibleForSheet);
+  BOOL shouldEmitHide = isKeyboardHiding
+    && (wasKeyboardVisibleForSheet || firstResponderWithinSheet);
+
+  if (!shouldEmitShow && !shouldEmitHide) {
+    return;
+  }
+
+  // iOS posts UIKeyboardWillChangeFrameNotification and UIKeyboardWillHideNotification
+  // for the same dismissal; dedupe within a single runloop tick.
+  if (shouldEmitHide) {
+    if (_isHandlingHide) {
+      return;
+    }
+    _isHandlingHide = YES;
+    __weak __typeof(self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+      __strong __typeof(weakSelf) strongSelf = weakSelf;
+      if (strongSelf) {
+        strongSelf->_isHandlingHide = NO;
+      }
+    });
+
+    [self smoothKeyboardHideWithDuration:duration curve:curve];
+  }
 
   _currentHeight = keyboardHeight;
+  _isKeyboardVisibleForSheet = !isKeyboardHiding;
 
   for (id<TrueSheetKeyboardObserverDelegate> delegate in _delegates) {
-    if (keyboardHeight > 0) {
+    if (shouldEmitShow) {
       [delegate keyboardWillShow:keyboardHeight duration:duration curve:curve];
-    } else {
+    } else if (shouldEmitHide) {
       [delegate keyboardWillHide:duration curve:curve];
     }
   }
