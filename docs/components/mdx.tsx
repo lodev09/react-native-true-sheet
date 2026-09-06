c53997baf253843203972fb04b02015e11a9a02e
Jovanni Lo <lodev09@gmail.com>
2026-09-07 05:37:36 +0800

    docs: fix dark sidebar tokens, tint callouts without shadow

--- docs/components/mdx.tsx
+++ docs/components/mdx.tsx
@@ -1,3 +1,4 @@
+import { CalloutContainer, type CalloutContainerProps } from 'fumadocs-ui/components/callout';
 import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
@@ -6,6 +7,12 @@ import type { HTMLAttributes } from 'react';
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    CalloutContainer: (props: CalloutContainerProps) => (
+      <CalloutContainer
+        {...props}
+        className="border-(--callout-color)/20 bg-(--callout-color)/10 shadow-none"
+      />
+    ),
     pre: (props: HTMLAttributes<HTMLPreElement>) => (
       <CodeBlock {...props} keepBackground>
         <Pre>{props.children}</Pre>
